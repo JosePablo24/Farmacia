e95a86e4928f121283b78b0b72c9c4387669d0f3
Talox1 <43228294+Talox1@users.noreply.github.com>
2020-07-03 07:56:11 -0500

    visualize and add new provider done

--- src/controller/ProveedoresController.java
+++ src/controller/ProveedoresController.java
@@ -5,20 +5,42 @@
  */
 package controller;
 
+import conections.Conexion;
+import far_system.LoginController;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.Person_system;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+
 /**
  * FXML Controller class
  *
@@ -29,31 +51,32 @@ public class ProveedoresController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
-    @FXML    private Button regresar;
+    private Conexion cc = new Conexion();
+    private Connection cn= cc.conexion();    
     Person_system person;
+        
+    @FXML    private Button regresar;
     
-    @FXML
-    private TextField nombreNuevoProv;
+    private ObservableList<ObservableList> data;
+    @FXML private TableView tablaProveedores;
+    
+    @FXML private TextField nombreNuevoProv;
 
-    @FXML
-    private TextField telefonoNuevoProv;
+    @FXML private TextField telefonoNuevoProv;
 
-    @FXML
-    private TextField rfcNuevoProv;
+    @FXML private TextField rfcNuevoProv;
 
-    @FXML
-    private TextField verNombreProv;
+    @FXML private TextField verNombreProv;
 
-    @FXML
-    private TextField verTelefonoProv;
+    @FXML private TextField verTelefonoProv;
 
-    @FXML
-    private TextField verRFCProv;
+    @FXML private TextField verRFCProv;
+    
+    @FXML private TextField verIdProv;
     
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        visualizateData();
     }
     
     @FXML
@@ -84,7 +107,35 @@ public class ProveedoresController implements Initializable {
 
     @FXML
     void OnMouseClickedNuevoProveedor(MouseEvent event) {
-    
+        if(nombreNuevoProv.getText().equals("") || telefonoNuevoProv.getText().equals("") || rfcNuevoProv.getText().equals("")){
+            Alert dialogAlert2 = new Alert(Alert.AlertType.WARNING);
+            dialogAlert2.setTitle("Advertencia");
+            dialogAlert2.setContentText("Hay Campos Vacios");
+            dialogAlert2.initStyle(StageStyle.UTILITY);
+            dialogAlert2.showAndWait();
+        }else{
+            String nombre = nombreNuevoProv.getText();
+            String telefono = telefonoNuevoProv.getText();
+            String rfc = rfcNuevoProv.getText();
+            String query = " INSERT INTO proveedores (Nombre, Rfc, Telefono)" +" Values (?, ?, ?)";
+            try {
+                PreparedStatement preparedStmt = cn.prepareStatement(query);
+                preparedStmt.setString (1, nombre);
+                preparedStmt.setString (2, rfc);
+                preparedStmt.setString(3, telefono);
+                preparedStmt.execute();
+                cn.close();
+                
+                Alert dialogAlert2 = new Alert(Alert.AlertType.CONFIRMATION);
+                dialogAlert2.setTitle("Exito");
+                dialogAlert2.setContentText("Se guardo con exito");
+                dialogAlert2.initStyle(StageStyle.UTILITY);
+                dialogAlert2.showAndWait();
+            } catch (SQLException e) {
+                System.err.println("\nError!... No se pudo realizar la sentencia");
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);            
+            }
+        }
     }
 
         
@@ -92,5 +143,53 @@ public class ProveedoresController implements Initializable {
         this.person = person;
     }
     
+    
+    public void visualizateData(){
+        String sql = "SELECT * FROM proveedores";
+        data = FXCollections.observableArrayList();
+        
+         try {            
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+                       
+             /**********************************
+             * TABLE COLUMN ADDED DYNAMICALLY *
+             **********************************/
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+                //We are using non property style for making dynamic table
+                final int j = i;                
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                        return new SimpleStringProperty(param.getValue().get(j).toString());                        
+                    }                    
+                });
+
+                tablaProveedores.getColumns().addAll(col); 
+                System.out.println("Column ["+i+"] ");
+            }
+            
+            /********************************
+             * Data added to ObservableList *
+             ********************************/
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added "+row );
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tablaProveedores.setItems(data);
+        } catch (SQLException e) {
+            System.err.println("\nMe llevo la ");
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);            
+        }
+    }
     
 }
