63fd49c02a8990b3cdab716ba4c1c51fb349d035
JosePablo24 <173198@ids.upchiapas.edu.mx>
2020-07-06 12:17:25 -0500

    prudctos: hecho por uriel

--- src/controller/ProductosController.java
+++ src/controller/ProductosController.java
@@ -14,9 +14,54 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import model.Person_system;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import conections.Conexion;
+
+import java.sql.Connection;
+
+import conections.Conexion;
+import far_system.LoginController;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import model.Person_system;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TablePosition;
+import javafx.scene.control.TableView.TableViewSelectionModel;
+
 
 /**
  * FXML Controller class
@@ -28,12 +73,57 @@ public class ProductosController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    @FXML    private Button regresar;
+	private Conexion cc = new Conexion();
+    private Connection cn = cc.conexion();
+    @FXML    
+    private Button regresar;
     Person_system person;
-    
+    private ObservableList<ObservableList> data;
+    @FXML
+    private TableView verTabla;
+    @FXML
+    private Button botonNuevo;
+    @FXML
+    private TextField nuevoNombrePro;
+    @FXML
+    private TextField nuevoDescripcionPro;
+    @FXML
+    private TextField nuevoCantidadPro;
+    @FXML
+    private TextField editarNombrePro;
+    @FXML
+    private TextField editarDescripcionPro;
+    @FXML
+    private TextField editarCantidadPro;
+    @FXML
+    private Button botonEditar;
+    @FXML
+    private Button botonEliminar;
+    @FXML
+    private TextField editarIDProv;
+    @FXML
+    private TextField nuevoIVAPro;
+    @FXML
+    private TextField nuevoPrecioPro;
+    @FXML
+    private TextField nuevoIDProvPro;
+    @FXML
+    private TextField editarIVAProv;
+    @FXML
+    private TextField editarPrecioPro;
+    @FXML
+    private TextField editarIDProvePro;
+    @FXML
+    private Button botonBuscar;
+    @FXML
+    private TextField buscarIDPro;
+    @FXML
+    private Button botonMostrar;
+
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    	visualizateData();
     }
     
     @FXML
@@ -50,9 +140,208 @@ public class ProductosController implements Initializable {
         Stage stage1 = (Stage) regresar.getScene().getWindow();
         stage1.close();
     }
+    @FXML
+    private void nuevo(ActionEvent event) {
+    	Conexion cc = new Conexion();
+        Connection cn = cc.conexion();
+        
+    	if(nuevoNombrePro.getText().equals("") || nuevoDescripcionPro.getText().equals("") || nuevoCantidadPro.getText().equals("") || nuevoIVAPro.getText().equals("") || nuevoPrecioPro.getText().equals("") || nuevoIDProvPro.getText().equals("")){
+            Alert dialogAlert2 = new Alert(Alert.AlertType.WARNING);
+            dialogAlert2.setTitle("Advertencia");
+            dialogAlert2.setContentText("Hay Campos Vacios");
+            dialogAlert2.initStyle(StageStyle.UTILITY);
+            dialogAlert2.showAndWait();
+        }else{
+            String nombre = nuevoNombrePro.getText();
+            String descripcion = nuevoDescripcionPro.getText();
+            String cantidad = nuevoCantidadPro.getText();
+            String iva = nuevoIVAPro.getText();
+            String precio = nuevoPrecioPro.getText();
+            String idProv = nuevoIDProvPro.getText();
+            String query = " INSERT INTO productos (Nombre, Descripcion, Cantidad, iva, Precio_unitario, Proveedores_id)" +" Values (?, ?, ?, ?, ?, ?)";
+            try {
+                PreparedStatement preparedStmt = cn.prepareStatement(query);
+                preparedStmt.setString(1, nombre);
+                preparedStmt.setString(2, descripcion);
+                preparedStmt.setString(3, cantidad);
+                preparedStmt.setString(4, iva);
+                preparedStmt.setString(5, precio);
+                preparedStmt.setString(6, idProv);
+                preparedStmt.execute();
+                cn.close();
+
+                Alert dialogAlert2 = new Alert(Alert.AlertType.CONFIRMATION);
+                dialogAlert2.setTitle("Exito");
+                dialogAlert2.setContentText("Se guardo con exito");
+                dialogAlert2.initStyle(StageStyle.UTILITY);
+                dialogAlert2.showAndWait();
+            } catch (SQLException e) {
+                System.err.println("\nError!... No se pudo realizar la sentencia");
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);            
+            }finally {
+            	updateInfoTable();
+            }
+        }
+    }
+
+    @FXML
+    private void editar(ActionEvent event) {
+    	Conexion cc = new Conexion();
+        Connection cn = cc.conexion();
+        
+    	if(editarNombrePro.getText().equals("") || editarDescripcionPro.getText().equals("") || editarCantidadPro.getText().equals("") || editarIVAProv.getText().equals("") || editarPrecioPro.getText().equals("") || editarIDProvePro.getText().equals("")){
+            Alert dialogAlert2 = new Alert(Alert.AlertType.WARNING);
+            dialogAlert2.setTitle("Advertencia");
+            dialogAlert2.setContentText("Hay Campos Vacios");
+            dialogAlert2.initStyle(StageStyle.UTILITY);
+            dialogAlert2.showAndWait();
+        }else{
+        	int id =Integer.parseInt(editarIDProv.getText());
+            String nombre = editarNombrePro.getText();
+            String descripcion = editarDescripcionPro.getText();
+            String cantidad = editarCantidadPro.getText();
+            String iva = editarIVAProv.getText();
+            String precio = editarPrecioPro.getText();
+            String idProv = editarIDProvePro.getText();
+            try {
+            	String query = " UPDATE productos SET Nombre = ?, Descripcion = ?, Cantidad = ?, iva = ?, Precio_unitario = ?, Proveedores_id = ? where id = ?";
+                PreparedStatement preparedStmt = cn.prepareStatement(query);
+                preparedStmt.setString(1, nombre);
+                preparedStmt.setString(2, descripcion);
+                preparedStmt.setString(3, cantidad);
+                preparedStmt.setString(4, iva);
+                preparedStmt.setString(5, precio);
+                preparedStmt.setString(6, idProv);
+                preparedStmt.setInt(7, id);
+                preparedStmt.execute();
+                cn.close();
+
+                Alert dialogAlert2 = new Alert(Alert.AlertType.CONFIRMATION);
+                dialogAlert2.setTitle("Exito");
+                dialogAlert2.setContentText("Se guardo con exito");
+                dialogAlert2.initStyle(StageStyle.UTILITY);
+                dialogAlert2.showAndWait();
+            } catch (SQLException e) {
+                System.err.println("\nError!... No se pudo realizar la sentencia");
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);            
+            }finally {
+            	updateInfoTable();
+            }
+        }
+    }
+
+    @FXML
+    private void eliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void buscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnMouseClcikedMostrarTodo(MouseEvent event) {
+    }
+
+    @FXML
+    private void mostrar(ActionEvent event) {
+    	updateInfoTable();
+    }
+    
+    private void updateInfoTable(){
+        String sql = "SELECT * FROM productos";
+        data = FXCollections.observableArrayList();
+        
+        try {
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                //System.out.println("Row [1] added "+row );
+                data.add(row);
+            }
+            
+            //FINALLY ADDED TO TableView
+            
+            verTabla.setItems(data);
+            verTabla.refresh();
+        } catch (Exception e) {
+
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    private void visualizateData(){
+        String sql = "SELECT * FROM productos";
+        data = FXCollections.observableArrayList();
+        
+        try {
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+                //We are using non property style for making dynamic table
+                final int j = i;                
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+                    
+                	public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                        return new SimpleStringProperty(param.getValue().get(j).toString());                        
+                    }                    
+                });
+
+                verTabla.getColumns().addAll(col); 
+                //System.out.println("Column ["+i+"] ");
+            }
+            
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                //System.out.println("Row [1] added "+row );
+                data.add(row);
+                
+            }
+
+            //FINALLY ADDED TO TableView
+            
+            verTabla.setItems(data);
+            //verTabla.refresh();
+        } catch (Exception e) {
+
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);
+        }
+        
+        verTabla.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observableValue, Object oldValue, Object newValue) {
+                //Check whether item is selected and set value of selected item to Label
+                if(verTabla.getSelectionModel().getSelectedItem() != null) {    
+                   //System.out.println(newValue);
+                   String []values = newValue.toString().replace("[", "").replace("]", "") .split(",");
+                   editarIDProv.setText(values[0]);
+                   editarNombrePro.setText(values[1]);
+                   editarDescripcionPro.setText(values[2]);
+                   editarCantidadPro.setText(values[3]);
+                   editarIVAProv.setText(values[4]);
+                   editarPrecioPro.setText(values[5]);
+                   editarIDProvePro.setText(values[6]);
+                }
+            }
+        });
+    }
     
     public void informacion(Person_system person){                
         this.person = person;
     }
+    
     
 }
